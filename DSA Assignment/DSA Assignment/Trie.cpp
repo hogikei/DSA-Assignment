a14fc3de6330025e1915688eb67a0ff3bddc8e39
Gi Kei Ho <s10179388@connect.np.edu.sg>
2019-01-27 23:01:00 +0800

    Comments

--- DSA Assignment/DSA Assignment/Trie.cpp
+++ DSA Assignment/DSA Assignment/Trie.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "string"
 // Converts key current character into index 
-// use only 'a' through 'z' and lower case 
+// Use only 'a' through 'z' and lower case 
 #define CHAR_TO_INDEX(c) ((int)c - (int)'a') 
 
 using namespace std;
@@ -11,48 +11,48 @@ using namespace std;
 // Iterative function to insert a key in the Trie
 void Trie::insert(std::string key)
 {
-	// start from root node
+	//Start from root node
 	Trie* curr = this;
 	for (int i = 0; i < key.length(); i++)
 	{
-		// create a new node if path doesn't exists
+		//Create a new node if path doesn't exists
 		if (curr->character[key[i]] == nullptr)
 			curr->character[key[i]] = new Trie();
 
-		// go to next node
+		//Go to next node
 		curr = curr->character[key[i]];
 	}
 
-	// mark current node as leaf
+	// Mrk current node as leaf
 	curr->isLeaf = true;
 	size += 1;
 }
 
 // Iterative function to search a key in Trie. It returns true
-// if the key is found in the Trie, else it returns false
+// If the key is found in the Trie, else it returns false
 bool Trie::search(std::string key)
 {
-	// return false if Trie is empty
+	//Return false if Trie is empty
 	if (this == nullptr)
 		return false;
 
 	Trie* curr = this;
 	for (int i = 0; i < key.length(); i++)
 	{
-		// go to next node
+		//Go to next node
 		curr = curr->character[key[i]];
 
-		// if string is invalid (reached end of path in Trie)
+		//If string is invalid (reached end of path in Trie)
 		if (curr == nullptr)
 			return false;
 	}
 
-	// if current node is a leaf and we have reached the
-	// end of the string, return true
+	//If current node is a leaf and we have reached the
+	//End of the string, return true
 	return curr->isLeaf;
 }
 
-// returns true if given node has any children
+//Returns true if given node has any children
 bool Trie::end(Trie const* curr)
 {
 	for (int i = 0; i < CHAR_SIZE; i++)
@@ -71,16 +71,20 @@ bool Trie::isEmpty()
 }
 
 
+//DisplayPrefix
 bool Trie::DisplayPrefix(string input, int length)
 {
-	//search for a prefix from trie
+	//Search for a prefix from trie
 	bool Validate = false;
+	//Create new node
 	Trie* trie = new Trie;
 
+	//Check if the node points to null
 	if (this == nullptr)
 		return false;
 	else
 	{
+		//Create current node
 		Trie* current = this;
 		for (int i = 0; i < length; i++) {
 			if (current->character[input[i]] == nullptr) {
@@ -99,9 +103,9 @@ bool Trie::DisplayPrefix(string input, int length)
 	}
 }
 
-
+//Traversal
 void Trie::Traversal(string input, Trie* trie) {
-	//online code to traverse 
+	//Online code to traverse 
 	for (int i = 0; i < CHAR_SIZE; i++) {
 		if (trie->character[i] != nullptr)  // if there is a value
 		{
